2cc8a24f5dbcd43efea9a0adac157e44145d137a
vpuhoff <vpuhoff@live.ru>
2016-09-16 23:51:52 +1000

    Stable

---
+++ DeadSectorKiller/PlotBox.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace DeadSectorKiller
+{
+    class PlotBox: PictureBox
+    {
+        Graphics gr;
+        Graphics bgr;
+        Bitmap buffer;
+        int cellH;
+        int cellW;
+        int Ycount;
+        int Xcount;
+        bool initReady = false;
+        public void CreateBox(int arraycount)
+        {
+            initReady = false;
+            gr = this.CreateGraphics();
+            buffer = new Bitmap(this.Width, this.Height);
+            bgr = Graphics.FromImage(buffer);
+            
+            double d = 0;
+            d = buffer.Width / buffer.Height;
+            double d2 = 0;
+            d2 = Math.Sqrt(arraycount);
+            d2 = d2 * d;
+            d2 = Math.Ceiling(d2);
+            Xcount = (int)d2;
+            d2 = arraycount / d2;
+            d2 = Math.Ceiling(d2);
+            Ycount = (int)d2;
+            cellH = (int)Math.Ceiling((double)((double)buffer.Height / (double)Ycount));
+            cellW = (int)Math.Ceiling((double)((double)buffer.Width / (double)Xcount));
+            bgr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            bgr.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            
+            for (int x = 0; x < Xcount ; x++)
+            {
+                for (int y = 0; y < Ycount; y++)
+                {
+                    bgr.FillRectangle(Brushes.Black  , x * cellW, y * cellH, cellW, cellH);
+                    bgr.DrawRectangle(pn, x * cellW, y * cellH, cellW, cellH);
+                }
+            }
+            gr.DrawImage(buffer, new Rectangle(0, 0, buffer.Width , buffer.Height ));
+            initReady = true;
+        }
+
+        Pen pn = new Pen(Brushes.DarkSlateBlue , (float)1.5);
+        double k = 0;
+        int xx, yy;
+        public void SetColor(int n, Color c)
+        {
+            if (initReady)
+            {
+                k = n / Xcount;
+                k = Math.Floor(k);
+                yy = (int)k;
+                yy = yy * cellH;
+                k = k * Xcount;
+                k = n - k;
+                xx = (int)k * cellW;
+                bgr.FillRectangle(new SolidBrush(c), xx, yy, cellW, cellH);
+                bgr.DrawRectangle(pn, xx, yy, cellW, cellH);
+                gr.FillRectangle(new SolidBrush(c), xx, yy, cellW, cellH);
+                gr.DrawRectangle(pn, xx, yy, cellW, cellH);
+                j++;
+                if (j%100==0)
+                {
+                    gr.DrawImage(buffer, new Rectangle(0, 0, buffer.Width, buffer.Height));
+                }
+            }
+            
+            
+        }
+
+        int j = 0;
+    }
+}
---
+++ DeadSectorKiller/ScanBox.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DeadSectorKiller
+{
+    partial class ScanBox
+    {
+        /// <summary> 
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Обязательный метод для поддержки конструктора - не изменяйте 
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotBox1 = new DeadSectorKiller.PlotBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pb1 = new System.Windows.Forms.ProgressBar();
+            this.pb2 = new System.Windows.Forms.ProgressBar();
+            this.pb3 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.plotBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plotBox1
+            // 
+            this.plotBox1.BackColor = System.Drawing.Color.Black;
+            this.plotBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plotBox1.Location = new System.Drawing.Point(0, 0);
+            this.plotBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.plotBox1.Name = "plotBox1";
+            this.plotBox1.Size = new System.Drawing.Size(463, 273);
+            this.plotBox1.TabIndex = 0;
+            this.plotBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.plotBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(463, 333);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.Black;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.pb3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.pb2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.pb1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 273);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(463, 60);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // pb1
+            // 
+            this.pb1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb1.Location = new System.Drawing.Point(1, 1);
+            this.pb1.Margin = new System.Windows.Forms.Padding(1);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(461, 18);
+            this.pb1.TabIndex = 0;
+            // 
+            // pb2
+            // 
+            this.pb2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb2.Location = new System.Drawing.Point(1, 21);
+            this.pb2.Margin = new System.Windows.Forms.Padding(1);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(461, 18);
+            this.pb2.TabIndex = 1;
+            // 
+            // pb3
+            // 
+            this.pb3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb3.Location = new System.Drawing.Point(1, 41);
+            this.pb3.Margin = new System.Windows.Forms.Padding(1);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(461, 18);
+            this.pb3.TabIndex = 2;
+            // 
+            // ScanBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ScanBox";
+            this.Size = new System.Drawing.Size(463, 333);
+            ((System.ComponentModel.ISupportInitialize)(this.plotBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PlotBox plotBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ProgressBar pb3;
+        private System.Windows.Forms.ProgressBar pb2;
+        private System.Windows.Forms.ProgressBar pb1;
+    }
+}
